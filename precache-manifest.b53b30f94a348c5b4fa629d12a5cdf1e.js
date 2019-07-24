29804c9d162608d6e67964888261d443c917586d
michal <michalwols@gmail.com>
2019-07-24 10:38:25 -0400

    Updates

---
+++ precache-manifest.b53b30f94a348c5b4fa629d12a5cdf1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca104564eecbc7b6c6d3676ca8c4c38f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8f4eb48b013b07031a9",
+    "url": "/static/css/main.58021e9f.chunk.css"
+  },
+  {
+    "revision": "b2c5b2b64195d2747c02",
+    "url": "/static/js/2.90a63d4a.chunk.js"
+  },
+  {
+    "revision": "e8f4eb48b013b07031a9",
+    "url": "/static/js/main.f625c36e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
